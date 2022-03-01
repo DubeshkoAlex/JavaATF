efb21cff1702874e1f3a65dbf875c54a99d7d2c2
Alexey Dubeshko <dubeshko.alexei@gmail.com>
2022-03-01 13:22:15 +0300

    add logging in CheckingEmailPage

--- src/test/java/page/youpmailPage/CheckingEmailPage.java
+++ src/test/java/page/youpmailPage/CheckingEmailPage.java
@@ -1,5 +1,7 @@
 package page.youpmailPage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,6 +14,7 @@ import java.util.List;
 public class CheckingEmailPage extends AbstractPage {
 
     private final String PAGE_URL = "https://yopmail.com/ru/wm";
+    private final Logger logger = LogManager.getRootLogger();
 
     @FindBy(id = "refresh")
     private WebElement refreshButton;
@@ -33,10 +36,13 @@ public class CheckingEmailPage extends AbstractPage {
     public String getCost(){
         int tryCount = 0;
         while (costInTheLetter.size()==0 && tryCount<5){
+            logger.info("try to refresh number: " + tryCount);
             driver.navigate().refresh();
             driver.switchTo().frame("ifmail");
             tryCount++;
         }
+        logger.info("all tryings are done, try ro get cost from the letter");
+        logger.info("Cost in the letter text: " + costInTheLetter.get(0).getText());
         return StringUtils.getValue(costInTheLetter.get(0),"(?<=USD\\s)[\\d,.]*");
     }
 
